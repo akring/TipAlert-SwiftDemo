5aedbceab14a3e275a1cc2eb89063cfd3bc36bfb
Akring <ddflj3210@gmail.com>
2015-09-30 15:48:48 +0800

    Initial Commit

---
+++ TipAlert-SwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipAlert-SwiftDemo
+//
+//  Created by 吕俊 on 15/9/30.
+//  Copyright © 2015年 Akring. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
